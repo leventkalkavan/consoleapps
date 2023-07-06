e89a704a2de8fe0545fae0911be0586f0f65e9b5
leventkalkavan <levent.kalkavan@hotmail.com>
2023-07-06 17:17:37 +0300

    tekrar eden sayilari bulma / tekrar eden sayilara yazi yazdırma

--- ConsoleApps/Program.cs
+++ ConsoleApps/Program.cs
@@ -1,3 +1,39 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
-Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] array = { 1, 1, 3, 4, 4, 2, 5, 6, 7, 8, 9, 9 };
+
+        var varElements = array.GroupBy(x => x)
+            .Where(g => g.Count() > 1)
+            .Select(g => g.Key);
+
+        Console.WriteLine("Tekrar eden elemanlar:");
+        foreach (int element in varElements)
+        {
+            string numText = GetNumberText(element);
+            Console.WriteLine(numText);
+        }
+
+        Console.ReadLine();
+    }
+
+    static string GetNumberText(int number)
+    {
+        switch (number)
+        {
+            case 1:
+                return "championss";
+            case 4:
+                return "fener";
+            case 9:
+                return "bahce";
+            default:
+                return number.ToString();
+        }
+    }
+}
